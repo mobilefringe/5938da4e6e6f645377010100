f2c54d045a86ff65352601da02fbb11db4d8c718
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-07-21 16:03:56 -0400

    Vue.js 2.0 Template 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -50,12 +50,7 @@
       },
       methods: {
         onOptionSelect(option) {
-            console.log('Selected option:', option)
-        },
-        removeDuplicates(myArr, prop) {
-            return myArr.filter((obj, pos, arr) => {
-                return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
-            });
+          console.log('Selected option:', option)
         }
       }
     })
